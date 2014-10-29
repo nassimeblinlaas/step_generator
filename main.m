073410dfffe4d716c8153fef628adca76d7d5013
mnaveau <maximilien.naveau@laas.fr>
2014-10-29 18:52:00 +0100

    check jacobian

--- main.m
+++ main.m
@@ -32,9 +32,9 @@ WAIST = 1;              % labels
 RLEG_JOINT5 = 7;        %
 LLEG_JOINT5 = 13;       %
 RARM_JOINT5 = 23;       %
-LARM_JOINT5 = 31;       %
+LARM_JOINT5 = 30;       %
 RARM_JOINT6 = 24;       %
-LARM_JOINT6 = 32;       %
+LARM_JOINT6 = 31;       %
 
 v_ref_B  = zeros(3,1);  % waist linear speed
 v_ref_F_1 = zeros(3,1);
@@ -193,14 +193,14 @@ while ( iteration < 10 )
     temp = eye(6,6);
     temp(1:3,4:6) = -r_B_H1;
     route = FindRoute(RARM_JOINT5);
-    J_arm_1 = CalcJacobian(route(:,3:end));
+    J_arm_1 = CalcJacobian(route);
     d_theta_arm_1 = J_arm_1\ xi_H_1 - J_arm_1\ temp * xi_B;
 
     % aem 2
     temp = eye(6,6);
     temp(1:3,4:6) = -r_B_H2;
     route = FindRoute(LARM_JOINT5);
-    J_arm_2 = CalcJacobian(route(:,4:end));
+    J_arm_2 = CalcJacobian(route);
     d_theta_arm_2 = J_arm_2\ xi_H_2 - J_arm_2\ temp * xi_B;
 
 
@@ -460,7 +460,7 @@ d_theta_arm_1 = J_arm_1\ xi_H_1 - J_arm_1\ temp * xi_B;
 temp = eye(6,6);
 temp(1:3,4:6) = -r_B_H2;
 route = FindRoute(LARM_JOINT5);
-J_arm_2 = CalcJacobian(route(:,4:end));
+J_arm_2 = CalcJacobian(route(:,3:end));
 d_theta_arm_2 = J_arm_2\ xi_H_2 - J_arm_2\ temp * xi_B;
 
 
