41568e50e3558d1d298b318ae2c2901e5fef63f0
nblin <nblin@ubuntu.(none)>
2014-10-31 10:23:44 +0100

    nothing really

--- main.m
+++ main.m
@@ -18,7 +18,7 @@ clear ;
 global uLINK G
 G = 9.81 ;
 
-number_of_samples = 2; % size of data to treat
+number_of_samples = 5; % size of data to treat
 pZ = 0.6487;            % position Z of robot constant
 period = 0.005;         % sampling period in seconds
 
@@ -190,7 +190,7 @@ for sample = 1 : number_of_samples
     v_ref_B   = v_CoM + cross(r_bc,w_ref_B) ;     % waist speed vector
     w_ref_B   = [ 0; 0; 0];
     
-    uLINK(WAIST).v = v_ref_B ;
+    uLINK(WAIST).v = v_ref_B;
     
     v_ref_F_1 = [ Data(sample, 14); Data(sample, 15); Data(sample, 16)];
     w_ref_F_1 = [ 0; 0; 0];
@@ -281,6 +281,8 @@ for sample = 1 : number_of_samples
         P  = PL(1:3);                           % linear momentum
         L  = PL(4:6);                           % angular momentum
 
+        
+        
         P_kajita(sample,:) = calcP(1);
         L_kajita(sample,:) = calcL(1);
     
@@ -312,13 +314,17 @@ for sample = 1 : number_of_samples
         P2 = PL2(1:3);                            % linear momentum
         L2 = PL2(4:6);                            % angular momentum
 
-        P
-        P2
-        P_kajita(sample,:)'
+        P;
+        P2;
+        P_kajita(sample,:)';
         
-        L
-        L2
-        L_kajita(sample,:)'
+        L;
+        L2;
+        L_kajita(sample,:)';
+        
+        dL = (L2 - L_prev) / period;             % finite difference method
+
+        L_prev = L2;                             % save previous value
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Step 4 : give \ddot{z_G}
